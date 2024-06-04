59865642a5acf93653ad7ba433b3d86ce32d36ca
Hunor Konczey <konczey.hunor@sonrisa.hu>
2024-06-04 08:45:06 +0300

    change heroku API URL

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:9000/api/"
+export const DEV_API_URL = "http://localhost:9000/api/"
+export const API_URL = "https://kotlin-docker-demo-6be97db4333f.herokuapp.com/api/"
 export const BANK_URL = API_URL + "banks/"
 export const USER_BANK_URL = API_URL + "banks/user/"
 export const TRANSACTION_URL = API_URL + "transactions/"
